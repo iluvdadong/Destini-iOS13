24826a93e70486a81d935a2fa8c2be6a1fc96243
Dahae Kim <iluvdadong@naver.com>
2020-07-26 19:37:26 +0900

    Step4 Update the Story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyNumber = 0
+    let stories = [
+        Story(t: "You see a fork in the road", c1: "Take a left", c2: "Take a right"),
+        Story(t: "You see a tiger", c1: "Shout for help", c2: "Play Dead"),
+        Story(t: "You find a treasure chest", c1: "Open it", c2: "Check for traps")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoice = sender.currentTitle!
+        
+        if userChoice == "Take a left" {
+            storyNumber += 1
+        } else {
+            storyNumber += 2
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        storyLabel.text = stories[storyNumber].text
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
+        
+    }
+    
 }
 
